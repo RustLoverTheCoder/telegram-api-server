70801942b51fc068ae9c767f799d0e62973107e7
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-05-20 22:51:29 +0800

    feat: add api core

---
+++ api/src/lib.rs
@@ -0,0 +1,40 @@
+use axum::{response::Html, routing::get, Router, Server};
+use core::sea_orm::Database;
+use migration::{Migrator, MigratorTrait};
+use std::str::FromStr;
+use std::{env, net::SocketAddr};
+
+#[tokio::main]
+async fn start() -> anyhow::Result<()> {
+    env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+
+    dotenvy::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{host}:{port}");
+
+    let conn = Database::connect(db_url)
+        .await
+        .expect("Database connection failed");
+    Migrator::up(&conn, None).await.unwrap();
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from_str(&server_url).unwrap();
+    Server::bind(&addr).serve(app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+pub fn main() {
+    let result = start();
+
+    if let Some(err) = result.err() {
+        println!("Error: {err}");
+    }
+}
