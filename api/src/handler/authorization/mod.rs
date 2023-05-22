91c7ab4ad97654e6add159bf7ae817e8316a8373
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-05-22 16:24:39 +0800

    feat: add authorization handler

---
+++ api/src/handler/authorization/mod.rs
@@ -0,0 +1,106 @@
+use anyhow::Error;
+
+// auth.bindTempAuthKey
+pub async fn auth_bind_temp_auth_key() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.cancelCode
+pub async fn auth_cancel_code() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.sendVerifyEmailCode
+pub async fn account_send_verify_email_code() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.verifyEmail
+pub async fn account_verify_email() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.checkPassword
+pub async fn auth_check_password() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.resetPassword
+pub async fn account_reset_password() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.checkRecoveryPassword
+pub async fn auth_check_recovery_password() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.dropTempAuthKeys
+pub async fn auth_drop_temp_auth_keys() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.exportAuthorization
+pub async fn auth_export_authorization() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.importAuthorization
+pub async fn auth_import_authorization() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.importBotAuthorization
+pub async fn auth_import_bot_authorization() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.changeAuthorizationSettings
+pub async fn account_change_authorization_settings() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.setAuthorizationTTL
+pub async fn account_set_authorization_ttl() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.logOut
+pub async fn auth_log_out() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.recoverPassword
+pub async fn auth_recover_password() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.requestPasswordRecovery
+pub async fn auth_request_password_recovery() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.resendCode
+pub async fn auth_resend_code() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.resetAuthorizations
+pub async fn auth_reset_authorizations() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.sendCode
+pub async fn auth_send_code() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.signIn
+pub async fn auth_sign_in() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// auth.signUp
+pub async fn auth_sign_up() -> Result<i32, Error> {
+    Ok(1)
+}
