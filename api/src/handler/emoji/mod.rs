47620808d7a111b88dae4a86a94d7766e0184f0c
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-05-23 21:50:59 +0800

    feat: add files

--- api/src/handler/emoji/mod.rs
+++ api/src/handler/emoji/mod.rs
@@ -19,3 +19,24 @@ pub async fn messages_get_emoji_keywords_languages() -> Result<i32, Error> {
 pub async fn messages_get_emoji_url() -> Result<i32, Error> {
     Ok(1)
 }
+
+
+// account.updateEmojiStatus
+pub async fn account_update_emoji_status() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.getDefaultEmojiStatuses
+pub async fn account_get_default_emoji_statuses() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.getRecentEmojiStatuses
+pub async fn account_get_recent_emoji_statuses() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.clearRecentEmojiStatuses
+pub async fn account_clear_recent_emoji_statuses() -> Result<i32, Error> {
+    Ok(1)
+}
---
+++ api/src/handler/files/mod.rs
@@ -0,0 +1,66 @@
+use anyhow::Error;
+
+// help.getCdnConfig
+pub async fn help_get_cdn_config() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.getCdnFile
+pub async fn upload_get_cdn_file() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.getCdnFileHashes
+pub async fn upload_get_cdn_file_hashes() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.reuploadCdnFile
+pub async fn upload_reupload_cdn_file() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.getFile
+pub async fn upload_get_file() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.getFileHashes
+pub async fn upload_get_file_hashes() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.getWebFile
+pub async fn upload_get_web_file() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.saveBigFilePart
+pub async fn upload_save_big_file_part() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// upload.saveFilePart
+pub async fn upload_save_file_part() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// messages.uploadEncryptedFile
+pub async fn messages_upload_encrypted_file() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// messages.uploadMedia
+pub async fn messages_upload_media() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// messages.getExtendedMedia
+pub async fn messages_get_extended_media() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// messages.getDocumentByHash
+pub async fn messages_get_document_by_hash() -> Result<i32, Error> {
+    Ok(1)
+}
