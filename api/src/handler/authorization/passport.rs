c0e9950879bcfd27e3024d1b8b0e8fd3ff1f2704
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-05-24 14:09:52 +0800

    feat: add handler

--- api/src/handler/authorization/passport.rs
+++ api/src/handler/authorization/passport.rs
@@ -14,3 +14,43 @@ pub async fn account_verify_phone() -> Result<i32, Error> {
 pub async fn account_accept_authorization() -> Result<i32, Error> {
     Ok(1)
 }
+
+// account.getAuthorizationForm	
+pub async fn account_get_authorization_form() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.getAuthorizations
+pub async fn account_get_authorizations() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.deleteSecureValue
+pub async fn account_delete_secure_value() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.getAllSecureValues
+pub async fn account_get_all_secure_values() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.getSecureValue
+pub async fn account_get_secure_value() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// account.saveSecureValue
+pub async fn account_save_secure_value() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// help.getPassportConfig
+pub async fn help_get_passport_config() -> Result<i32, Error> {
+    Ok(1)
+}
+
+// users.setSecureValueErrors
+pub async fn users_set_secure_value_errors() -> Result<i32, Error> {
+    Ok(1)
+}
