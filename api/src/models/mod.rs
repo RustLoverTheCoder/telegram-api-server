860a552f1fe1de2dd3d7de3198b6aa79e6985e42
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-07-12 14:55:56 +0800

    feat: add user model

--- api/src/models/mod.rs
+++ api/src/models/mod.rs
@@ -1,12 +1,2 @@
 pub mod chat;
-
-use std::{collections::HashSet, sync::Mutex};
-use tokio::sync::broadcast;
-
-// Our shared state
-pub struct AppState {
-    // We require unique usernames. This tracks which usernames have been taken.
-    pub user_set: Mutex<HashSet<String>>,
-    // Channel used to send messages to all connected clients.
-    pub tx: broadcast::Sender<String>,
-}
+pub mod user;
