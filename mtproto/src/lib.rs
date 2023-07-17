3cf999feec7cd6a0340cf814b405069729bc2b77
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-07-17 22:47:56 +0800

    feat: add mt service

--- mtproto/src/lib.rs
+++ mtproto/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// use grammers_mtproto::{ MtProtoService, BoxedFuture, Request, Response };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// struct MyService;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// impl MtProtoService for MyService {
+
+//     fn process(&self, req: &Request) -> BoxedFuture<Response> {
+
+//         let response = match req.body() {
+//             // 处理auth.sendCode请求
+//             grammers_tl_types::req_pq::AuthSendCode {..} => {
+//                 // TODO: 实现发送验证码逻辑
+//                 let sent_code = "...发送的验证码...";
+//                 let res = grammers_tl_types::ResPQ {
+//                     nonce: req.nonce(),
+//                     server_nonce: vec![1,2,3],
+//                     pq: vec![1,2,3],
+//                     new_nonce: vec![4,5,6],
+//                 };
+
+//                 Response::ok(res)
+//             }
+
+//             // 处理auth.signIn请求
+//             grammers_tl_types::req_pq::AuthSignIn {..} => {
+//                 // TODO: 验证验证码和签名等信息
+
+//                 Response::ok(grammers_tl_types::AuthAuthorization {
+//                     expires: 3600,
+//                     user: grammers_tl_types::User {
+//                         id: 1234,
+//                         ..Default::default()
+//                     }
+//                 })
+//             }
+
+//             // 其他请求
+//             _ => Response::err(RpcError::MethodNotFound)
+//         };
+
+//         Box::pin(async move {
+//             response
+//         })
+//     }
+// }
